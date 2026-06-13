dbbec0bf99188b6c9bacb7e1516f0a596c36c06d
Saibal-Tony <berasaibal20@gmail.com>
2026-06-13 08:47:46 +0530

    dockerfile

--- shopsphere-backend/src/main/java/com/shopsphere/backend/config/SecurityConfig.java
+++ shopsphere-backend/src/main/java/com/shopsphere/backend/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://shopsphere-2-c421vtl9p-saibal-tonys-projects.vercel.app/" // add your Vercel URL
+                "https://shopsphere-shop.vercel.app/" // add your Vercel URL
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
